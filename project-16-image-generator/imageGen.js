6f93f8143af6d83ddfb06f78d14c410ef852dc2b
markruth2nd <119530770+markruth2nd@users.noreply.github.com>
2023-04-19 19:09:19 +0100

    finished day/night mode function which will be called when user loads screen

---
+++ project-16-image-generator/imageGen.js
@@ -0,0 +1,32 @@
+const input = document.getElementById("inpur");
+const grid = document.getElementsByClassName("grid")[0];
+
+//The below will call the dayNightMode function as soon as the page loads
+window.addEventListener("load", dayNightMode);
+
+
+input.addEventListener('keydown', function(event){
+    if(event.key === "Enter")
+    loadImg();
+})
+
+function loadImg(){
+
+}
+
+//The below function will remove all images from the screen, this will be used when the user puts in a new search.
+
+//The below is the function in which I will have the webpage auto change the day or night mode by using Date() & getHours() functions within JS. Then I will apply an if statement using the date and time for JS to render the page in day mode by amending the css style if hours between 7am - 19pm, otherwise it will display the page in nightmode.
+function dayNightMode(){
+    const date = new Date();
+    const hour = date.getHours();
+
+    if(hour >= 7 && hour <= 19){
+        document.body.style.backgroundColor = "whitesmoke";
+        document.body.style.color = "black";
+    }
+    else{
+        document.body.style.backgroundColor = "black";
+        document.body.style.color = "white";
+    }
+}
